41766caca2fd7cd8048cac6cd7c51eb1132ba12d
emaanuelsilvaa <emanuelborgesdasilva@gmail.com>
2021-07-19 21:57:48 -0400

    initial commit

---
+++ GCMProjectAPI/gcm/src/main/java/br/com/imd/gcmproject/gcm/GcmApplication.java
@@ -0,0 +1,13 @@
+package br.com.imd.gcmproject.gcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcmApplication.class, args);
+	}
+
+}
